d42c519d39f65ea5223fc0c94ebd4d984af1804e
vidapogosoft <vidapogosoft@hotmail.com>
2022-09-19 21:15:37 -0500

    Base de datos local Sqlite

---
+++ SQLite1/SQLite1/Model/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+using SQLite;
+
+namespace SQLite1.Model
+{
+    public class Student
+    {
+        [PrimaryKey, AutoIncrement]
+        public int stdid { get; set; }
+        [NotNull]
+        public string stdname { get; set; }
+        [NotNull]
+        public string stdcourse { get; set; }
+        public string stdlevel { get; set; }
+    }
+}
---
+++ SQLite1/SQLite1/Views/AddStudent.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SQLite1.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AddStudent : ContentPage
+    {
+        public AddStudent()
+        {
+            InitializeComponent();
+        }
+    }
+}
