2fcf89e1475d3ce8604c78f5ba5a677ad42a6506
vidapogosoft <vidapogosoft@hotmail.com>
2022-10-03 20:39:46 -0500

    Call in xamarin forms web services asmx

---
+++ TodoASMX/TodoASMX/Constants.cs
@@ -0,0 +1,24 @@
+﻿using Xamarin.Forms;
+
+namespace TodoASMX
+{
+    public static class Constants
+    {
+        // URL of ASMX service
+        public static string SoapUrl
+        {
+            get
+            {
+                var defaultUrl = "http://localhost:49178/TodoService.asmx";
+
+                if (Device.RuntimePlatform == Device.Android)
+                {
+                    //defaultUrl = "http://10.0.2.2:49178/TodoService.asmx";
+                    defaultUrl = "http://192.168.100.10/todoasmx/TodoService.asmx";
+                }
+
+                return defaultUrl;
+            }
+        }
+    }
+}
