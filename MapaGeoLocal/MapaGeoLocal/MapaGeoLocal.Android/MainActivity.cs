61711c1da00d998c0bd44eced1aea11ec7f9c9cd
vidapogosoft <vidapogosoft@hotmail.com>
2023-05-30 15:28:44 -0500

    varios cambios

--- MapaGeoLocal/MapaGeoLocal/MapaGeoLocal.Android/MainActivity.cs
+++ MapaGeoLocal/MapaGeoLocal/MapaGeoLocal.Android/MainActivity.cs
@@ -12,7 +12,7 @@ using Plugin.CurrentActivity;
 
 namespace MapaGeoLocal.Droid
 {
-    [Activity(Label = "MapaGeoLocal", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "GeoLocal", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
